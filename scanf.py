c41372f1440fb8f3e2382956c81822ff64e69029
Daniel Verner <danielv2000@gmail.com>
2024-05-15 23:54:06 +0200

    basic poc for coverting code

--- scanf.py
+++ scanf.py
@@ -14,10 +14,12 @@ def check_scanf(node, declared_vars, current_function):
         print(f"Error in {current_function}(): insufficient arguments passed to scanf (More format strings than args)")
         return
     elif len(format_parts) < len(additional_args):
-        print(f"Warning in {current_function}(): Excessive arguments passed to scanf (More args than format strings)")
+        print(f"Error in {current_function}(): Excessive arguments passed to scanf (More args than format strings)")
 
     for index, part in enumerate(format_parts):
         current_arg = additional_args[index].name
+        array_info = declared_vars[current_arg]
+        array_size = array_info['size']
         if not current_arg in declared_vars:
             print(f'Error in {current_function}(): {current_arg} is not declared')
             return 
@@ -25,11 +27,13 @@ def check_scanf(node, declared_vars, current_function):
         max_chars_str = ''.join(filter(str.isdigit, part))
         if(len(max_chars_str) == 0):
             print(f'Warning in {current_function}: no max length specified on argument %{part} at index: {index}')
+            print("Modifying to correct max size")
+            node.args.exprs[index] = c_ast.Constant(type='string', value=f'"%{array_size - 1}{part}"')
         else:
             
             format_max_size = int(max_chars_str)
             # the size can be at most size -1 of the buffer. This is because there needs to be one byte reserved for the null terminator
-            array_size = declared_vars[current_arg]['size']
+            
             if(format_max_size == array_size):
                 print(f'Warning in {current_function}(): Format string size %{part} should account for null terminator byte (max size = buf_size - 1 = {array_size - 1})')
             elif(format_max_size > array_size):
